cc4fbef6d5a6cc9dce3dc83fc59fba97f08adbe5
Stephen Michael <stephen@tucsonstephen.com>
2022-08-06 10:27:27 -0700

    Initial Commit

---
+++ CarLIst/CarLIstApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarLIstApp.swift
+//  CarLIst
+//
+//  Created by Office Mac on 8/6/22.
+//
+
+import SwiftUI
+
+@main
+struct CarLIstApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CarLIst/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CarLIst
+//
+//  Created by Office Mac on 8/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
